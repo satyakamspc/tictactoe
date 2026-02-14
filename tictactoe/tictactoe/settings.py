868f9dd48f5a0561fc47a9144c8d4e9ed92a30cd
satyakam <satyakam26@gmail.com>
2026-02-14 22:51:17 +0530

    changed in allowed hosts

--- tictactoe/tictactoe/settings.py
+++ tictactoe/tictactoe/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-utmo=seqazbr+-49q212+ur+)_nf_ty=kd46u**j50c1e&jtmc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
