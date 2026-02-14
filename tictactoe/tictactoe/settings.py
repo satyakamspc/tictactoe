49932a693fa49f9d086dcbef803789daeb924c08
satyakam <satyakam26@gmail.com>
2026-02-15 00:14:53 +0530

    chan

--- tictactoe/tictactoe/settings.py
+++ tictactoe/tictactoe/settings.py
@@ -114,7 +114,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 import os
-STATIC_URL = 'static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
-STATIC_ROOT = [
-    os.path.join(BASE_DIR,"staticfiles")]
+STATIC_URL = '/static/'
+STATICFILES_DIRS=[BASE_DIR/'static',]
+STATIC_ROOT = BASE_DIR/"staticfiles"
