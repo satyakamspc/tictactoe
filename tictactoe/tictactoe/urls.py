34d2eaa08ed103a442e82c50a8b8252aaee51310
satyakam <satyakam26@gmail.com>
2026-02-14 23:43:22 +0530

    SMALL CHANGE

--- tictactoe/tictactoe/urls.py
+++ tictactoe/tictactoe/urls.py
@@ -19,5 +19,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index,name='index')
+    path('',views.layout,name='layout')
 ]
